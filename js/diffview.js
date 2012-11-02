2d1ea541b218def47f6098b4e81c1234d209fe51
soe <soe@soe.im>
2012-11-02 21:05:58 +0100

    v 0.2

--- js/diffview.js
+++ js/diffview.js
@@ -122,6 +122,7 @@ diffview = {
 		function addCellsInline (row, tidx, tidx2, textLines, change) {
 			row.appendChild(telt("th", tidx == null ? "" : (tidx + 1).toString()));
 			row.appendChild(telt("th", tidx2 == null ? "" : (tidx2 + 1).toString()));
+			console.log(textLines[tidx]);
 			//row.appendChild(ctelt("td", change, textLines[tidx != null ? tidx : tidx2].replace(/\t/g, "\u00a0\u00a0\u00a0\u00a0")));
 			row.appendChild(ctelt("td", change, textLines[tidx != null ? tidx : tidx2].replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;")));
 		}

a5c97f5599651a373b39648b9604faaf61a47e96
soe <soe@soe.im>
2012-11-03 01:18:59 +0100

    v 0.2

--- js/diffview.js
+++ js/diffview.js
@@ -110,7 +110,7 @@ diffview = {
 			if (tidx < tend) {
 				row.appendChild(telt("th", (tidx + 1).toString()));
 				//row.appendChild(ctelt("td", change, textLines[tidx].replace(/\t/g, "\u00a0\u00a0\u00a0\u00a0")));
-				row.appendChild(ctelt("td", change, textLines[tidx]));
+				row.appendChild(ctelt("td", change, textLines[tidx].replace(/\t/g, "&#x09;")));
 				return tidx + 1;
 			} else {
 				row.appendChild(document.createElement("th"));
@@ -123,7 +123,7 @@ diffview = {
 			row.appendChild(telt("th", tidx == null ? "" : (tidx + 1).toString()));
 			row.appendChild(telt("th", tidx2 == null ? "" : (tidx2 + 1).toString()));
 			//row.appendChild(ctelt("td", change, textLines[tidx != null ? tidx : tidx2].replace(/\t/g, "\u00a0\u00a0\u00a0\u00a0")));
-			row.appendChild(ctelt("td", change, textLines[tidx != null ? tidx : tidx2]));
+			row.appendChild(ctelt("td", change, textLines[tidx != null ? tidx : tidx2].replace(/\t/g, "&#x09;")));
 		}
 		
 		for (var idx = 0; idx < opcodes.length; idx++) {
@@ -194,6 +194,6 @@ diffview = {
 		
 		node = celt("table", "diff" + (inline ? " inlinediff" : ""));
 		for (var idx in tdata) node.appendChild(tdata[idx]);
-		return node;
+		return node.replace(/\s/g, '&nbsp;');
 	}
 }

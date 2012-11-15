d331528eccc5be432141673fa6e2199cdfbb4c84
soe <soe@soe.im>
2012-11-15 19:20:52 +0100

    opcodes

--- js/diffview.js
+++ js/diffview.js
@@ -215,16 +215,16 @@ diffview = {
 			for(var i = 0; i < rowcnt; i++) {
 				if(change == "insert") {
 					lhs.push('<td></td><td class="skip">&nbsp;</td>'); // skip
-					rhs.push('<td>'+ n+i+1 +'</td><td class="insert">'+ newTextLines[n + i] +'</td>');
+					rhs.push('<td>'+ parseInt(n+i+1) +'</td><td class="insert">'+ newTextLines[n + i] +'</td>');
 				} else if(change == "delete") {
-					lhs.push('<td>'+ b+i+1 +'</td><td class="delete">'+ baseTextLines[b + i] +'</td>');
+					lhs.push('<td>'+ parseInt(b+i+1) +'</td><td class="delete">'+ baseTextLines[b + i] +'</td>');
 					rhs.push('<td></td><td class="skip">&nbsp;</td>'); // skip
 				} else if(change == "replace") {
-					if(be <= b + i) lhs.push('<td>'+ b+i+1 +'</td><td class="replace">'+ baseTextLines[b + i] +'</td>');
-					if(ne <= n + i) rhs.push('<td>'+ n+i+1 +'</td><td class="replace">'+ newTextLines[n + i] +'</td>');
+					if(be <= b + i) lhs.push('<td>'+ parseInt(b+i+1) +'</td><td class="replace">'+ baseTextLines[b + i] +'</td>');
+					if(ne <= n + i) rhs.push('<td>'+ parseInt(n+i+1) +'</td><td class="replace">'+ newTextLines[n + i] +'</td>');
 				} else if(change == "equal") {
-					lhs.push('<td>'+ b+i+1 +'</td><td class="equal">'+ baseTextLines[b + i] +'</td>');
-					rhs.push('<td>'+ n+i+1 +'</td><td class="equal">'+ newTextLines[n + i] +'</td>');
+					lhs.push('<td>'+ parseInt(b+i+1) +'</td><td class="equal">'+ baseTextLines[b + i] +'</td>');
+					rhs.push('<td>'+ parseInt(n+i+1) +'</td><td class="equal">'+ newTextLines[n + i] +'</td>');
 				}	
 			}
 		} // end for

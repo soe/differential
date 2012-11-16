6eb7a02893ebeba1d705d1d303a861d42244e425
soe <soe@soe.im>
2012-11-16 01:15:37 +0100

    v 2

--- js/difflib.js
+++ js/difflib.js
@@ -44,8 +44,8 @@ difflib = {
 
 		// spaceChar & tabChar
 		str = str.replace(/[\n|\r][ |\t]+/g, function(x) { 
-			if(params.spaceChar) x = x.replace(/ /g, '<span class="whitechar">'+ params.spaceChar +'</span>'); 
-			if(params.tabChar) x = x.replace(/\t/g, '<span class="whitechar">'+ params.tabChar +'</span>'); 
+			if(params.spaceChar) x = '<span class="whitechar">'+ x.replace(/ /g, params.spaceChar) +'</span>'; 
+			if(params.tabChar) x = '<span class="whitechar">'+ x.replace(/\t/g, params.tabChar) +'</span>'; 
 
 			return x
 		});
